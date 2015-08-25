52cdd33667fb68df1e4cfb730f6c380794002a88
peterschretlen <peter.schretlen@gmail.com>
2015-08-24 23:34:20 -0400

    initialize db in provisioning script

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ http://bdr-project.org/docs/0.9.0/installation-packages.html
 It uses the 2creatives centos 6.5 box as a base
 https://github.com/2creatives/vagrant-centos/releases/tag/v6.5.3
 
+To create a two node environement
+```
+vagrant up
+```
+
+To destroy/clean up both VMs
+```
+vagrant destroy --force
+```
--- provision/setup_bdr.sh
+++ provision/setup_bdr.sh
@@ -9,3 +9,9 @@ sudo yum install -q -y http://packages.2ndquadrant.com/postgresql-bdr94-2ndquadr
 echo "Install the BDR packages"
 
 sudo yum install -q -y postgresql-bdr94-bdr
+
+echo "Add postgres to path"
+echo "PATH=/usr/pgsql-9.4/bin:$PATH" >> /etc/profile
+
+echo "Initialize DB"
+sudo /usr/pgsql-9.4/bin/postgresql94-setup initdb -D /var/lib/pgsql/9.4-bdr/data
